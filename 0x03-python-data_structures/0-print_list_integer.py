045f1816720b814f9d3492236fae5985671c204f
Amodu Olaribigbe <scubesept@gmail.com>
2023-08-15 14:03:52 -0700

    python data structure project

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print all integers of a list
+# -----------------------------------------------------------
+
+
+def print_list_integer(my_list=[]):
+    """Prints all intergers of a list
+
+    Args:
+      my_list: the list argument whose items will be printed
+    """
+
+    # Iterate through list
+    for integer in my_list:
+        print("{:d}".format(integer))
